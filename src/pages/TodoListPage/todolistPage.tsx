7c3b581b67098fdbeaab95ece1358095d4fdefd4
Konstantin <kapurelloruru@gmail.com>
2025-07-31 16:25:17 +0300

    naming fixed

--- src/pages/TodoListPage/todolistPage.tsx
+++ src/pages/TodoListPage/todolistPage.tsx
@@ -10,7 +10,7 @@ import type { FilterStatus, Todo, TodoInfo } from "../../types/todos";
 
 type LoadingStatus = "idle" | "pending" | "succeed" | "failed";
 
-export const Asdasd = () => {
+export const TodoListPage = () => {
   const [todosData, setTodosData] = useState<Todo[]>([]);
   const [todosInfo, setTodosInfo] = useState<TodoInfo>({
     all: 0,
