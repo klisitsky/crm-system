08d32d3aa3241fa8edc3f8854c4fdfcc36ff236b
Konstantin <kapurelloruru@gmail.com>
2025-07-31 16:19:28 +0300

    bug fixed

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,9 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, redirect, RouterProvider } from "react-router";
 import "./index.css";
 import "./styles/index.scss";
-import App from "./App";
-import { ProfilePage } from "./pages/ProfilePage/ProfilePage";
-import { TodoListPage } from "./pages/TodoListPage/TodoListPage";
+import App from "./App.tsx";
+import { ProfilePage } from "./pages/ProfilePage/ProfilePage.tsx";
+import { TodoListPage } from "./pages/TodoListPage/todolistPage.tsx";
 
 const router = createBrowserRouter([
   {
