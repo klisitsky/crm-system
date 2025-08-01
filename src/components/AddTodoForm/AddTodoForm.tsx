80f28e2a5e8abe952e558152d2c9ff441c204412
Konstantin <kapurelloruru@gmail.com>
2025-08-01 13:03:37 +0300

    fixed forms validation and api refactoring

--- src/components/AddTodoForm/AddTodoForm.tsx
+++ src/components/AddTodoForm/AddTodoForm.tsx
@@ -11,63 +11,65 @@ interface AddTodoForm {
 export const MIN_SYMBOLS_COUNT = 2;
 export const MAX_SYMBOLS_COUNT = 64;
 
-export const AddTodoForm: React.FC<AddTodoForm> = memo(({ isLoading, onUpdate }) => {
-  const [form] = Form.useForm();
+export const AddTodoForm: React.FC<AddTodoForm> = memo(
+  ({ isLoading, onUpdate }) => {
+    const [form] = Form.useForm();
 
-  const handleAddTodo = useCallback(() => {
-    form.validateFields().then(async (res) => {
-      createTodo(res.todoValue.trim())
-        .then(() => {
-          onUpdate?.();
-          form.resetFields();
-        })
-        .catch((err) => {
-          alert(getErrorMessage(err));
-        });
-    });
-  }, [form, onUpdate]);
+    const handleCreateTodo = useCallback(() => {
+      form.validateFields().then((res) => {
+        createTodo(res.todoValue)
+          .then(() => {
+            onUpdate?.();
+            form.resetFields();
+          })
+          .catch((err) => {
+            alert(getErrorMessage(err));
+          });
+      });
+    }, [form, onUpdate]);
 
-  return (
-    <Form form={form} validateTrigger="none">
-      <Flex gap="large" justify="center">
-        <Form.Item
-          style={{ margin: 0 }}
-          name="todoValue"
-          rules={[
-            {
-              required: true,
-              message: "Поле не может быть пустым",
-            },
-            {
-              min: MIN_SYMBOLS_COUNT,
-              message: "Длина менее 2 символов",
-            },
-            {
-              max: MAX_SYMBOLS_COUNT,
-              message: "Длина более 64 символов",
-            },
-          ]}
-        >
-          <Input
-            placeholder="Todo To Be Done..."
-            disabled={isLoading}
-            variant="underlined"
-            size="middle"
-            style={{ backgroundColor: "transparent" }}
-          />
-        </Form.Item>
-        <Form.Item style={{ margin: 0 }}>
-          <Button
-            type="primary"
-            onClick={handleAddTodo}
-            disabled={isLoading}
-            style={{ width: "100px" }}
-            htmlType="submit"
+    return (
+      <Form form={form} validateTrigger="none" onFinish={handleCreateTodo}>
+        <Flex gap="large" justify="center">
+          <Form.Item
+            style={{ margin: 0 }}
+            name="todoValue"
+            rules={[
+              {
+                required: true,
+                message: "Поле не может быть пустым",
+                transform: (value) => value.trim(),
+              },
+              {
+                min: MIN_SYMBOLS_COUNT,
+                message: "Длина менее 2 символов",
+              },
+              {
+                max: MAX_SYMBOLS_COUNT,
+                message: "Длина более 64 символов",
+              },
+            ]}
           >
-            Add
-          </Button>
-        </Form.Item>
-      </Flex>
-    </Form>
-  );
-})
+            <Input
+              placeholder="Todo To Be Done..."
+              disabled={isLoading}
+              variant="underlined"
+              size="middle"
+              style={{ backgroundColor: "transparent" }}
+            />
+          </Form.Item>
+          <Form.Item style={{ margin: 0 }}>
+            <Button
+              type="primary"
+              disabled={isLoading}
+              style={{ width: "100px" }}
+              htmlType="submit"
+            >
+              Add
+            </Button>
+          </Form.Item>
+        </Flex>
+      </Form>
+    );
+  }
+);
