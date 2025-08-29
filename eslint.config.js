31b6512d16124e26af0fbbaf325817ae4e64a2e5
Konstantin <kapurelloruru@gmail.com>
2025-08-29 15:44:44 +0300

    fixed eslint

--- eslint.config.js
+++ eslint.config.js
@@ -20,8 +20,8 @@ export default tseslint.config([
       globals: globals.browser,
     },
     rules: {
-      "no-unused-vars": "warn",
-      "@typescript-eslint/no-unused-vars": "warn"
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "off"
     }
   },
 ]);
