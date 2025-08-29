db33bcc2ae8b908ecedacfdb53b8928d5bcc2e4f
Konstantin <kapurelloruru@gmail.com>
2025-08-29 15:57:41 +0300

    fixed typing

--- eslint.config.js
+++ eslint.config.js
@@ -20,8 +20,8 @@ export default tseslint.config([
       globals: globals.browser,
     },
     rules: {
-      "no-unused-vars": "off",
-      "@typescript-eslint/no-unused-vars": "off"
+      "no-unused-vars": "warn",
+      "@typescript-eslint/no-unused-vars": "warn"
     }
   },
 ]);
